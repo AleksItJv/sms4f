e61c2982981fc3e596f4fba9065a0880132aa94c
unknown <aleks.it.jv@gmail.com>
2022-05-06 12:37:38 +0300

    start project

---
+++ src/main/java/ua/sms4f/Sms4fApplication.java
@@ -0,0 +1,13 @@
+package ua.sms4f;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sms4fApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sms4fApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/sms4f/Sms4fApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.sms4f;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sms4fApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
